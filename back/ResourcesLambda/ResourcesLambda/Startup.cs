9e923b390b7d50bf0fe77344ec68be557f2afd67
jviaches <jviaches@gmail.com>
2020-03-17 20:00:57 -0500

    resolve CORS

--- back/ResourcesLambda/ResourcesLambda/Startup.cs
+++ back/ResourcesLambda/ResourcesLambda/Startup.cs
@@ -41,6 +41,8 @@ namespace ResourcesLambda
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
