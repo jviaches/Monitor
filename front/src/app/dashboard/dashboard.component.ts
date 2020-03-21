ad932b3a52454b801bb6848e946198a18baefe62
jviaches <jviaches@gmail.com>
2020-03-21 09:20:16 -0500

    Closing #6, resource details

--- front/src/app/dashboard/dashboard.component.ts
+++ front/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { IResource } from '../core/models/resource.model';
 import { Chart } from 'angular-highcharts';
-import { Options } from 'highcharts';
+import { Options, PointOptionsObject, Point } from 'highcharts';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,98 +17,28 @@ export class DashboardComponent implements OnInit {
   activeResources: IResource[] = [];
   inActiveResources: IResource[] = [];
 
-  charts: Chart[] = [];
+  chartMap: Map<number, Chart>;
+
   panelOpenState = false;
 
   // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Date';
-  showYAxisLabel = true;
-  yAxisLabel = 'Status Code';
-  timeline = false;
-  autoScale = true;
-  options: Options;
+  // showXAxis = true;
+  // showYAxis = true;
+  // gradient = false;
+  // showLegend = false;
+  // showXAxisLabel = true;
+  // xAxisLabel = 'Date';
+  // showYAxisLabel = true;
+  // yAxisLabel = 'Status Code';
+  // timeline = false;
+  // autoScale = true;
+  // options: Options;
 
-  color: ThemePalette = 'accent';
-  checked = true;
-  disabled = false;
+  // color: ThemePalette = 'accent';
+  // checked = true;
+  // disabled = false;
 
-  chartOptions: any;
-
-  chart = new Chart({
-    chart: {
-      type: 'line',
-    },
-    title: {
-      text: 'Monitoring'
-    },
-    subtitle: {
-      text: 'Period: Jan 2019 - Dec 2019'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [{
-      type: 'line',
-      name: 'http://www.stack.com',
-      data: [{
-        name: 'Point 1',
-        color: '#00FF00',
-        y: 200
-      }, {
-        name: 'Point 2',
-        color: '#FF00FF',
-        y: 500
-      },
-      {
-        name: 'Point 2',
-        color: '#FF00FF',
-        y: 400
-      }],
-      color: '#FF0000'
-    },
-    {
-      type: 'line',
-      name: 'http://www.techflask.com',
-      data:
-        [{
-          name: 'Point 1',
-          color: '#A0DB8E',
-          y: 400
-        }, {
-          name: 'Point 2',
-          color: '#A0DB8E',
-          y: 400
-        },
-        {
-          name: 'Point 2',
-          color: '#A0DB8E',
-          y: 400
-        }],
-      color: '#A0DB8E'
-    }],
-    xAxis: {
-      title: {
-        text: 'Status'
-      },
-      // categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-      title: {
-        text: 'Status'
-      },
-      //  labels: {
-      //   formatter() {
-      //     const foundValue =  [200, 400, 404, 500, 504].find(el => el === this.value);
-      //     return foundValue ? foundValue + '' : '';
-      //   }
-      // },
-    },
-  });
+  // chartOptions: any;
 
   constructor(private resourceService: ResourceService) {
   }
@@ -115,6 +46,7 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.resourceService.getResources(1).subscribe(resource => {
       this.resources = resource;
+      this.buildHistoryStatusChart(resource);
       this.activeResources = this.resources.filter(res => res.isMonitorActivated);
       this.inActiveResources = this.resources.filter(res => !res.isMonitorActivated);
     });
@@ -124,6 +56,45 @@ export class DashboardComponent implements OnInit {
     console.log(event);
   }
 
+  private buildHistoryStatusChart(resources: IResource[]) {
+    this.chartMap = new Map<number, Chart>();
 
+    resources.forEach(element => {
+
+      const historyData: any[] = [];
+      element.history.map(record => historyData.push( ({ name: record.responseDate, y: Number(record.result) })));
+
+      this.chartMap[element.id] = new Chart({
+        chart: {
+          type: 'line',
+        },
+        title: {
+          text: 'Monitoring History'
+        },
+        // subtitle: {
+        //   text: 'Period: Jan 2019 - Dec 2019'
+        // },
+        credits: {
+          enabled: false
+        },
+        series: [{
+          type: 'line',
+          name: element.url,
+          data: historyData,
+          color: '#FF0000'
+        }],
+        xAxis: {
+          title: {
+            text: 'Timeline'
+          },
+          categories: historyData.map( cat => cat.name),
+        },
+        yAxis: {
+          title: {
+            text: 'Status Code'
+          },
+        },
+      });
+    });
+  }
 }
-
