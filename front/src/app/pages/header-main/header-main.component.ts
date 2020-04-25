34aaf6dbe871ecaeb04dead50cdbd8026c6e2b50
jviaches <jviaches@gmail.com>
2020-04-25 09:12:32 -0500

    header recognize if user logged in or not and show appropriate links

--- front/src/app/pages/header-main/header-main.component.ts
+++ front/src/app/pages/header-main/header-main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthorizationService } from 'src/app/core/services/authentication.service';
+import { GeneralService } from 'src/app/core/services/general.service';
 
 @Component({
   selector: 'app-header-main',
@@ -8,8 +10,22 @@ import { Router } from '@angular/router';
 })
 export class HeaderMainComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  isAuthorized = false;
+
+  constructor(private router: Router, private authorizationService: AuthorizationService,
+              private generalService: GeneralService) { }
 
   ngOnInit() {
+    if (this.authorizationService.isLoggedIn()) {
+      this.isAuthorized = true;
+    }
+  }
+
+  logOff() {
+    this.generalService.showYesNoModalMessage().subscribe(data => {
+      if (data === 'yes') {
+        this.authorizationService.logOut();
+      }
+    });
   }
 }
