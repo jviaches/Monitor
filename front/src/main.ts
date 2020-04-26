702cdddb6b405e8d75ffcff2188378087bd1f574
jviaches <jviaches@gmail.com>
2020-04-25 19:33:20 -0500

    deployment fix

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,12 +2,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from '../src/environments/environment';
+import { environment } from 'environments/environment';
 
 // Amplify Configuration
 import Auth from '@aws-amplify/auth';
 import Storage from '@aws-amplify/storage';
-import AWSConfig from '../src/aws-exports';
+import AWSConfig from './aws-exports';
 
 // Storage.configure(AWSConfig);
 Auth.configure(AWSConfig);
