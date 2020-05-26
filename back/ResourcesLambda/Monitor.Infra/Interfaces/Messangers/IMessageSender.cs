28e4670dc5d8660b9402fc0603b3bc504ae5d0de
jviaches <jviaches@gmail.com>
2020-05-25 21:51:14 -0500

    hard coded Slack message preparation

---
+++ back/ResourcesLambda/Monitor.Infra/Interfaces/Messangers/IMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor.Infra.Interfaces.Messangers
+{
+    public interface IMessageSender
+    {
+        Task SendMessage(string channel, string message);
+    }
+}
