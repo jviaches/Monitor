9ba41790201692476d41ca782f9648bbab639b8a
jviaches <jviaches@gmail.com>
2020-05-29 10:18:54 -0500

    no message

--- front/src/app/environments/environment.staging.ts
+++ front/src/app/environments/environment.staging.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  api: 'https://0kob1kuomh.execute-api.us-east-2.amazonaws.com/Prod/api/',
+  // api: 'https://0kob1kuomh.execute-api.us-east-2.amazonaws.com/Prod/api/',         // Lamda func address
+  api: 'http://monitorstaging-env.eba-qfmij5dy.us-east-2.elasticbeanstalk.com/api/',  // Beanstack address
   production: false,
 };
