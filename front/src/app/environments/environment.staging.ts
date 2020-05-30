3f4d4cf0efe7379fc4dfa09af9e050d6ceaa7681
jviaches <jviaches@gmail.com>
2020-05-30 10:04:19 -0500

    Revert "no message"
    This reverts commit 9ba41790201692476d41ca782f9648bbab639b8a.

--- front/src/app/environments/environment.staging.ts
+++ front/src/app/environments/environment.staging.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // api: 'https://0kob1kuomh.execute-api.us-east-2.amazonaws.com/Prod/api/',         // Lamda func address
-  api: 'http://monitorstaging-env.eba-qfmij5dy.us-east-2.elasticbeanstalk.com/api/',  // Beanstack address
+  api: 'https://0kob1kuomh.execute-api.us-east-2.amazonaws.com/Prod/api/',
   production: false,
 };
